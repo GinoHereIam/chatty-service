ccd03f8c766e193647e986a0e48f82833f855dcd
GinoHereIam <ga@gino-atlas.de>
2017-09-02 17:28:40 +0200

    Client: * Restructred assets * Generate styles, pictures, index.html

---
+++ src/main/resources/public/assets/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWPPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: {
+        app: './app/app.js'
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'source-map',
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM",
+        "re-bulma": "re-bulma"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract('css-loader')
+            },
+            {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+            {test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/},
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWPPlugin({
+            title: 'Chatty - self-hosting chat',
+            template: 'template/index.ejs',
+            inject: 'body'
+        }),
+        new ExtractTextPlugin("style.[hash].css")
+    ]
+};
